fd9c6c57d40c26f0f170faae034f004c9b58b26f
Kuzumi <s9271@pjwstk.edu.pl>
2013-09-03 22:18:31 +0200

    mala przerobka logowania

--- jee6demo/src/main/java/com/example/jeedemo/admin/adminAccount.java
+++ jee6demo/src/main/java/com/example/jeedemo/admin/adminAccount.java
@@ -1,19 +1,29 @@
 package com.example.jeedemo.admin;
 
 public class adminAccount{
-	private String login = "admin";
-	private String password = "test";
+	private String login;
+	private String password;
 	
-	public adminAccount (String login, String password){
+	/*public adminAccount (String login, String password){
 		this.login = login;
+		this.password = password;
+	}*/
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
 		this.password = password;
 	}
 	
-	public String getLogin(){
-		return this.login;
-	}
 	
-	public String getPassword(){
-		return this.password;
-	}
 }
--- jee6demo/src/main/java/com/example/jeedemo/admin/adminForm.java
+++ jee6demo/src/main/java/com/example/jeedemo/admin/adminForm.java
@@ -1,29 +1,53 @@
 package com.example.jeedemo.admin;
 
-/*import java.io.Serializable;
+import java.io.Serializable;
 
 import javax.enterprise.context.SessionScoped;
-import javax.inject.Inject;
+//import javax.inject.Inject;
 import javax.inject.Named;
 
 @SessionScoped
-@Named("adminForm")*/
-public class adminForm{
-	/*private String login = "admin";
-	private String password = "test";
+@Named("adminForm")
+public class adminForm implements Serializable{
+	private static final long serialVersionUID = 1L; //ni uja nie wiem co to
+	private String loginForm = "admin";
+	private String passwordForm = "test";
+	private String login;
+	private String password;
 	
-	public Account(String login, String password){
+	/*private adminAccount adminAccount = new adminAccount();
+	
+	public adminAccount getadminAccount() {
+		return adminAccount;
+	}
+	public void setadminAccount(adminAccount adminAccount) {
+		this.adminAccount = adminAccount;
+	}*/
+	
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
 		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
 		this.password = password;
 	}
 	
-	public String getLogin(){
-		return this.login;
+	//Akcje
+	public String signin() {
+		if(this.loginForm.equals(login) && this.passwordForm.equals(password)){
+			return "showPersons";
+		}else{
+			return null;
+		}
 	}
-	
-	public String getPassword(){
-		return this.password;
-	}*/
-}
 
-//problem z klasa
+	
+}
